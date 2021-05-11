59969f6d6ac77e5c5b3cccdccf8ed30f74cc2970
JonShort <JonShort@users.noreply.github.com>
2021-05-11 09:31:22 +0100

    Make node script somewhat functional

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const { parse_env } = require('./pkg/cenv_wasm');
+import { readFile, writeFile } from 'fs/promises';
+import { parse_env } from './pkg/cenv_wasm.js';
 
-const mockContents = `
-# ++ one ++
-# TEST_A=1
-# TEST_B=1
+const FILE = '.example';
 
-# ++ two ++
-TEST_A=2
-TEST_B=2
+const logFsErrorAndExit = (err) => {
+  console.log("\x1b[31m%s\x1b[0m", "[Error] - Error while reading", FILE, "file");
+  console.log("\x1b[31m%s\x1b[0m", " |", "Failed with the following error:");
+  console.log("\x1b[31m%s\x1b[0m", " |", err.message);
+  process.exit(1);
+};
 
-# ++ three ++
-# TEST_A=3
-# TEST_B=3
-`;
+const currentEnv = await readFile(FILE, { encoding: 'utf-8' }).catch(logFsErrorAndExit)
 
-console.log(parse_env(mockContents, "three"))
+const providedKeyword = process.argv[2];
+
+if (!providedKeyword) {
+  console.log("\x1b[31m%s\x1b[0m", "[Error] - No keyword provided");
+  console.log("\x1b[31m%s\x1b[0m", " |", "Please provide your keyword as the first argument to cenv");
+  console.log("\x1b[31m%s\x1b[0m", " |", "e.g. `cenv myKeyword`");
+  process.exit(1);
+}
+
+await writeFile(FILE, parse_env(currentEnv, providedKeyword)).catch(logFsErrorAndExit)
+
+console.log("Updated", FILE, "to", providedKeyword);
+process.exit(0);
