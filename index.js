90ca91cbb9477c553d2f923ea5018d6067cb4fe3
JonShort <JonShort@users.noreply.github.com>
2021-05-17 21:56:38 +0100

    Refactor JS to rely primarily on the wasm

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
 #!/usr/bin/env node
-const { readFile, writeFile } = require('fs/promises');
-const { parse_env } = require('./pkg/cenv_wasm.js');
+const { main } = require('./pkg/cenv_wasm.js');
 
-const FILE = '.env';
-
-const logFsErrorAndExit = (err) => {
-  console.log("\x1b[31m%s\x1b[0m", "[Error] - Error while reading", FILE, "file");
-  console.log("\x1b[31m%s\x1b[0m", " |", "Failed with the following error:");
-  console.log("\x1b[31m%s\x1b[0m", " |", err.message);
+const logErrorAndExit = (err, description = "Failed with the following error:") => {
+  console.log("\x1b[31m%s\x1b[0m", "[Error] - Error while running");
+  console.log("\x1b[31m%s\x1b[0m", " |", description);
+  console.log("\x1b[31m%s\x1b[0m", " |", err);
   process.exit(1);
 };
 
-const main = async () => {
-  const currentEnv = await readFile(FILE, { encoding: 'utf-8' }).catch(logFsErrorAndExit)
-
+const jsMain = async () => {
   const providedKeyword = process.argv[2];
 
   if (!providedKeyword) {
-    console.log("\x1b[31m%s\x1b[0m", "[Error] - No keyword provided");
-    console.log("\x1b[31m%s\x1b[0m", " |", "Please provide your keyword as the first argument to cenv");
-    console.log("\x1b[31m%s\x1b[0m", " |", "e.g. `cenv myKeyword`");
-    process.exit(1);
+    logErrorAndExit("e.g. `cenv myKeyword`", "Please provide your keyword as the first argument to cenv");
+    return;
   }
 
   let newEnv;
   try {
-    newEnv = parse_env(currentEnv, providedKeyword)
+    newEnv = main(providedKeyword)
   } catch (err) {
-    console.log("\x1b[31m%s\x1b[0m", "[Error] - Error while running");
-    console.log("\x1b[31m%s\x1b[0m", " |", "Failed with the following error:");
-    console.log("\x1b[31m%s\x1b[0m", " |", err);
-    process.exit(1);
+    logErrorAndExit(err);
+    return;
   }
 
-  await writeFile(FILE, newEnv).catch(logFsErrorAndExit)
-
-  console.log("Updated", FILE, "to", providedKeyword);
   process.exit(0);
 }
 
-main();
+jsMain();
