f485d7fd1b3fbf83e3e3c36e0fd1810eeea65ca8
JonShort <JonShort@users.noreply.github.com>
2021-05-18 08:37:23 +0100

    Update JS to just call main

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 #!/usr/bin/env node
 const { main } = require('./pkg/cenv_wasm.js');
 
-const logErrorAndExit = (err, description = "Failed with the following error:") => {
+const logErrorAndExit = (err) => {
   console.log("\x1b[31m%s\x1b[0m", "[Error] - Error while running");
-  console.log("\x1b[31m%s\x1b[0m", " |", description);
+  console.log("\x1b[31m%s\x1b[0m", " |", "Failed with the following error:");
   console.log("\x1b[31m%s\x1b[0m", " |", err);
   process.exit(1);
 };
 
-const jsMain = () => {
-  const providedKeyword = process.argv[2];
-
-  if (!providedKeyword) {
-    logErrorAndExit("e.g. `cenv myKeyword`", "Please provide your keyword as the first argument to cenv");
-    return;
-  }
-
-  let newEnv;
-  try {
-    newEnv = main(providedKeyword)
-  } catch (err) {
-    logErrorAndExit(err);
-    return;
-  }
-
+try {
+  newEnv = main()
   process.exit(0);
+} catch (err) {
+  logErrorAndExit(err);
 }
-
-jsMain();
